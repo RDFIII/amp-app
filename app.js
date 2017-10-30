0d0ae21700a8319119cb684b34d3401d3739ce28
RDF <foulkesr@gmail.com>
2017-10-30 15:36:46 -0400

    added mongodb and a few fake data entries, mongoose too

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/amp-app", { useMongoClient: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let amps = [
-  {name: "Vox", image: "https://media.sweetwater.com/images/items/750/AC30VR-large.jpg?45f280cb98"},
-  {name: "Fender", image: "https://media.sweetwater.com/api/i/ha-af1d6c5a0d50f7bf__q-82__hmac-5bc8e421029c14ab4702bb899e6b0ee92f5d703b/images/items/750/65DeluxeRev-large.jpg"},
-  {name: "Marshall", image: "https://cdn.guitarchalk.com/wp-content/uploads/2016/07/Marshall-DSL40C-40-Watt-Combo-Amp-clipped.png"},
-  {name: "Vox", image: "https://media.sweetwater.com/images/items/750/AC30VR-large.jpg?45f280cb98"},
-  {name: "Fender", image: "https://media.sweetwater.com/api/i/ha-af1d6c5a0d50f7bf__q-82__hmac-5bc8e421029c14ab4702bb899e6b0ee92f5d703b/images/items/750/65DeluxeRev-large.jpg"},
-  {name: "Marshall", image: "https://cdn.guitarchalk.com/wp-content/uploads/2016/07/Marshall-DSL40C-40-Watt-Combo-Amp-clipped.png"},
-  {name: "Vox", image: "https://media.sweetwater.com/images/items/750/AC30VR-large.jpg?45f280cb98"},
-  {name: "Fender", image: "https://media.sweetwater.com/api/i/ha-af1d6c5a0d50f7bf__q-82__hmac-5bc8e421029c14ab4702bb899e6b0ee92f5d703b/images/items/750/65DeluxeRev-large.jpg"},
-  {name: "Marshall", image: "https://cdn.guitarchalk.com/wp-content/uploads/2016/07/Marshall-DSL40C-40-Watt-Combo-Amp-clipped.png"},
-  {name: "Vox", image: "https://media.sweetwater.com/images/items/750/AC30VR-large.jpg?45f280cb98"},
-  {name: "Fender", image: "https://media.sweetwater.com/api/i/ha-af1d6c5a0d50f7bf__q-82__hmac-5bc8e421029c14ab4702bb899e6b0ee92f5d703b/images/items/750/65DeluxeRev-large.jpg"},
-  {name: "Marshall", image: "https://cdn.guitarchalk.com/wp-content/uploads/2016/07/Marshall-DSL40C-40-Watt-Combo-Amp-clipped.png"},
-];
+// SCHEMA SETUP
+let ampSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+let Amp = mongoose.model("Amp", ampSchema);
+
+// Amp.create({
+//   name: "Marshall", image: "https://cdn.guitarchalk.com/wp-content/uploads/2016/07/Marshall-DSL40C-40-Watt-Combo-Amp-clipped.png"
+// }, function(err, amp){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log("NEWLY CREATED AMP");
+//     console.log(amp);
+//   };
+// });
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/amps", function(req, res){
-  res.render("amps", {amps: amps});
+  // res.render("amps", {amps: amps});
+  Amp.find({}, function(err, allAmps){
+    if(err){
+      console.log("ERROR");
+    } else {
+      res.render("amps", {amps: allAmps});
+    };
+  });
 });
 
 app.get("/amps/new", function(req, res){
@@ -36,8 +50,13 @@ app.post("/amps", function(req, res){
   let name = req.body.name;
   let image = req.body.image;
   let newAmp = {name: name, image: image};
-  amps.push(newAmp);
-  res.redirect("/amps");
+  Amp.create(newAmp, function(err, newlyCreated){
+    if(err){
+      console.log("ERROR");
+    } else {
+      res.redirect("/amps");
+    };
+  });
 });
 
 app.listen(3000, function(){
