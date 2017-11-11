f687b89e4dd21eed3a8237125e2807c2cf09f3e7
RDF <foulkesr@gmail.com>
2017-11-11 10:12:15 -0500

    navbar functional, shows username

--- app.js
+++ app.js
@@ -52,7 +52,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -65,7 +68,7 @@ app.get("/amps", function(req, res){
     if(err){
       console.log("ERROR");
     } else {
-      res.render("amps/index", {amps: allAmps});
+      res.render("amps/index", {amps: allAmps, currentUser: req.user});
     };
   });
 });
