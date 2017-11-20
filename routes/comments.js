06fe118fc5efaa170b569b05cabb176da6c0c814
RDF <foulkesr@gmail.com>
2017-11-20 18:31:39 -0500

    added delete functionality for comments

--- routes/comments.js
+++ routes/comments.js
@@ -61,6 +61,17 @@ router.put("/:comment_id", function(req, res){
   });
 });
 
+// DESTROY
+router.delete("/:comment_id", function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("back");
+    };
+  });
+});
+
 // Middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
